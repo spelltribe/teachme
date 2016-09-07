47ceecad1e8c3de31839f331c06cefa96a59aa44
vagrant <vagrant@homestead>
2016-09-07 21:17:03 +0000

    Ticket and TicketVote new models and migrations

---
+++ database/migrations/2016_09_07_211212_create_ticket_votes_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_votes', function (Blueprint $table) {
+            $table->increments('id');
+
+            // Columnas agregadas
+           
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->integer('ticket_id')->unsigned();
+            $table->foreign('ticket_id')->references('id')->on('tickets');
+
+            //
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket_votes');
+    }
+}
